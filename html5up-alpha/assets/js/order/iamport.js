a5ecd76199fe4732d581935018f7acd7fa2260d1
LEEJONGHYEON <ditlswlwhs3@gmail.com>
2022-08-26 17:31:55 +0900

    order 페이지 수정

--- html5up-alpha/assets/js/order/iamport.js
+++ html5up-alpha/assets/js/order/iamport.js
@@ -1,20 +1,46 @@
 var IMP = window.IMP; 
 IMP.init("imp00383227");
-var flower = ($("#import_flower"));
-
+var addr= new Map([
+    ["구례산림 조합 장례식장","전남 구례군 구례읍 용방로 15"],
+    ["구례 병원 장례식장","전남 구례군 구례읍 동편제길 4 의료시설"],
+    ["구례 효사랑 요양병원 장례식장","전남 구례군 구례읍 봉동길 31"],
+    ["새미소 요양병원 장례식장","전남 구례군 구례읍 구례로 28 지리산요양병원"]
+])
+var postcode= new Map([
+    ["구례산림 조합 장례식장","57634"],
+    ["구례 병원 장례식장","57645"],
+    ["구례 효사랑 요양병원 장례식장","57637"],
+    ["새미소 요양병원 장례식장","57659"]
+])
+function test(){
+    var place=$("#funeral_name").val();
+    console.log($("#flower_info").children().eq(0).children().text());
+    console.log($("#flower_info").children().eq(2).children().text());
+    console.log($("#send_name").val());
+    console.log($("#send_phone").val());
+    console.log(get_addr(place));
+    console.log(get_postcode(place));
+}
+function get_addr(place){
+    return addr.get(place)+", "+$("#mortuary").val();
+}
+function get_postcode(place){
+    return postcode.get(place);
+}
 function requestPay() {
+    var place=$("#funeral_name").val();
     // IMP.request_pay(param, callback) 결제창 호출
     IMP.request_pay({ // param
         pg: "html5_inicis",
         pay_method: "card",
         merchant_uid: "ORD20180131-0000011",
-        name: flower,
-        amount: 77000,
+        name: $("#import_flower").children().text(),
+        amount: $("#import_flower").children().text(),
         buyer_email: "gildong@gmail.com",
-        buyer_name: "홍길동",
-        buyer_tel: "010-4242-4242",
-        buyer_addr: "서울특별시 강남구 신사동",
-        buyer_postcode: "01181"
+        buyer_name: $("#send_name").val(),
+        buyer_tel: $("#send_phone").val(),
+        buyer_addr: get_addr(place),
+        buyer_postcode: get_postcode(place)
     }, function (rsp) { // callback
         if (rsp.success) {
        
